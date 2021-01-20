c94ef29e87a64740f4bd460f07eba683c2bd9567
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2021-01-21 11:26:31 +1300

    name / age program

---
+++ 01_variables.py
@@ -0,0 +1,8 @@
+# code to learn about variables
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+# greet user
+greeting = "Hello {}.  You are {}".format(name, age)
+print(greeting)
